e9118c1bb7048d49d7667948a147d271aea84dab
Агеев Виталий <vitaliy.ageev@rktv.ru>
2022-03-11 07:23:14 +0500

    Dev App

--- atrade/src/components/TradingIdeas/Idea/Content/Main/Chart/Chart.jsx
+++ atrade/src/components/TradingIdeas/Idea/Content/Main/Chart/Chart.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef } from 'react';
 import classes from "./Chart.module.css";
 import { createChart, LineStyle, Range } from 'lightweight-charts';
 
-const Chart = ({ data }) => {
+const Chart = ({ initialData, barsData, isTrue }) => {
     const chartContainerRef = useRef();
 
     useEffect(
@@ -28,15 +28,15 @@ const Chart = ({ data }) => {
                 crosshair: {
                     mode: 0,
                     vertLine: {
-                        visible: false,
-                        width: 0,
+                        visible: true,
+                        width: 1,
                         color: 'rgba(0, 0, 0, 0.2)',
                         style: 0,
-                        labelVisible: false,
+                        labelVisible: true,
                     },
                     horzLine: {
-                        visible: false,
-                        labelVisible: false,
+                        visible: true,
+                        labelVisible: true,
                         width: 1,
                         color: 'rgba(0, 0, 0, 0.2)',
                         style: 0,
@@ -58,15 +58,15 @@ const Chart = ({ data }) => {
                         top: 0.1,
                         bottom: 0.1,
                     },
-                    alignLabels: false,
-                    borderColor: 'rgba(0,0,0, 0.2)',
+                    alignLabels: true,
+                    borderColor: 'rgba(0,0,0, 0.1)',
                 },
                 timeScale: {
                     visible: true,
                     // fixLeftEdge: true,
                     // fixRightEdge: true,
                     rightOffset: 2.5,
-                    borderColor: 'rgba(0,0,0, 0.2)',
+                    borderColor: 'rgba(0,0,0, 0)',
 
                 },
                 handleScroll: {
@@ -81,88 +81,137 @@ const Chart = ({ data }) => {
                     axisPressedMouseMove: false,
                     axisDoubleClickReset: false,
                     mouseWheel: false,
-                },
+                }
             });
 
+            if (isTrue === true) {
+                const newSeries = chart.addAreaSeries({
+                    lineColor: 'rgba(34, 160, 83, 1)',
+                    topColor: 'rgba(224, 31, 25, .4)',
+                    bottomColor: 'rgba(224, 31, 25, .01)',
+                    priceFormat: {
+                        minMove: 0.00001,
+                    },
+                    // title: 'Buy',
+                    lineColor: 'rgba(38, 167, 154, 0.8)',
+                    priceLineSource: 2,
+                    topColor: 'rgba(38, 167, 154, .3)',
+                    bottomColor: 'rgba(38, 167, 154, 0)',
+                    lastPriceAnimation: 2,
+                    // lastValueVisible: false,
 
-            // const newSeries = chart.addAreaSeries({
-            //     lineColor: 'rgba(34, 160, 83, 1)',
-            //     topColor: 'rgba(224, 31, 25, .4)',
-            //     bottomColor: 'rgba(224, 31, 25, .01)',
-            //     priceFormat: {
-            //         minMove: 0.00001,
-            //     },
-            //     // title: 'Buy',
-            //     lineColor: 'rgba(38, 167, 154, 0.8)',
-            //     priceLineSource: 2,
-            //     topColor: 'rgba(38, 167, 154, .3)',
-            //     bottomColor: 'rgba(38, 167, 154, 0)',
-            //     lastPriceAnimation: 2,
-            //     // lastValueVisible: false,
+                    priceLineVisible: false,
+                    lastValueVisible: false,
+                    priceLineWidth: 2,
+                    priceLineStyle: LineStyle.Dashed,
+                    // lineColor: 'rgba(0, 120, 255, 1)',
+                    // topColor: 'rgba(0, 120, 255, 0.4)',
+                    // bottomColor: 'rgba(0, 120, 255, 0.01)',
+                    lineWidth: 2,
+                    autoScaleInfoProvider: {
+                        priceRange: {
+                            minValue: 2,
+                            maxValue: 2,
+                        },
+                    },
+                    crosshairMarkerVisible: false,
+                });
 
-            //     priceLineVisible: false,
-            //     lastValueVisible: false,
-            //     priceLineWidth: 2,
-            //     priceLineStyle: LineStyle.Dashed,
-            //     // lineColor: 'rgba(0, 120, 255, 1)',
-            //     // topColor: 'rgba(0, 120, 255, 0.4)',
-            //     // bottomColor: 'rgba(0, 120, 255, 0.01)',
-            //     lineWidth: 2,
-            //     autoScaleInfoProvider: {
-            //         priceRange: {
-            //             minValue: 2,
-            //             maxValue: 2,
-            //         },
-            //     },
-            //     crosshairMarkerVisible: false,
-            // });
+                newSeries.priceScale().width()
+                newSeries.setData(initialData)
 
-            const newSeries = chart.addCandlestickSeries({
-                lastValueVisible: false,
-                priceLineVisible: false,
-                priceFormat: {
-                    minMove: 0.00001,
-                },
+                const currentPrice = {
+                    price: 1.115,
+                    color: 'rgba(38, 167, 154, 0)',
+                    // color: 'rgba(0,0,0,0)',
+                    lineWidth: '2',
+                    lineStyle: LineStyle.Dashed,
+                    axisLabelVisible: true,
+                    title: 'Покупка',
+                }
 
-            });
+                const takeProfit = {
+                    price: 1.135,
+                    // color: 'rgba(43, 92, 250, 0)',
+                    color: 'rgba(38, 167, 154, 0)',
+                    // color: 'rgba(0,0,0,0)',
+                    lineWidth: '2',
+                    lineStyle: LineStyle.Dashed,
+                    axisLabelVisible: true,
+                    title: 'Цель',
+                }
 
+                const stopLoss = {
+                    price: 1.08495,
+                    color: 'rgba(247, 40, 40, 0)',
+                    // color: 'rgba(0,0,0,0)',
+                    lineWidth: '2',
+                    lineStyle: LineStyle.Dashed,
+                    axisLabelVisible: true,
+                    title: 'Стоп',
+                }
 
+                newSeries.createPriceLine(stopLoss);
+                newSeries.createPriceLine(takeProfit);
+                newSeries.createPriceLine(currentPrice);
 
-            newSeries.priceScale().width()
-            newSeries.setData(data)
+            } else {
 
+                const newSeries = chart.addCandlestickSeries({
+                    lastValueVisible: false,
+                    priceLineVisible: false,
+                    priceFormat: {
+                        minMove: 0.00001,
+                    },
+                    borderVisible: true,
 
-            const currentPrice = {
-                price: 1.115,
-                color: 'rgba(38, 167, 154, 0)',
-                lineWidth: '2',
-                lineStyle: LineStyle.Dashed,
-                axisLabelVisible: true,
-                title: 'Покупка',
+                    // upColor: '#fff',
+                    // borderUpColor: '#000',
+                    // downColor: '#000',
+                    // borderDownColor: '#000',
+                    // wickColor: 'rgba(0,0,0, 0.7)'
+
+                });
+
+                newSeries.priceScale().width()
+                newSeries.setData(barsData)
+
+                const currentPrice = {
+                    price: 1.115,
+                    color: 'rgba(38, 167, 154, 0)',
+                    // color: 'rgba(0,0,0,0)',
+                    lineWidth: '2',
+                    lineStyle: LineStyle.Dashed,
+                    axisLabelVisible: true,
+                    title: 'Покупка',
+                }
+
+                const takeProfit = {
+                    price: 1.135,
+                    // color: 'rgba(43, 92, 250, 0)',
+                    color: 'rgba(38, 167, 154, 0)',
+                    // color: 'rgba(0,0,0,0)',
+                    lineWidth: '2',
+                    lineStyle: LineStyle.Dashed,
+                    axisLabelVisible: true,
+                    title: 'Цель',
+                }
+
+                const stopLoss = {
+                    price: 1.08495,
+                    color: 'rgba(247, 40, 40, 0)',
+                    // color: 'rgba(0,0,0,0)',
+                    lineWidth: '2',
+                    lineStyle: LineStyle.Dashed,
+                    axisLabelVisible: true,
+                    title: 'Стоп',
+                }
+
+                newSeries.createPriceLine(stopLoss);
+                newSeries.createPriceLine(takeProfit);
+                newSeries.createPriceLine(currentPrice);
+
             }
-
-            const takeProfit = {
-                price: 1.135,
-                // color: 'rgba(43, 92, 250, 0)',
-                color: 'rgba(38, 167, 154, 0)',
-                lineWidth: '2',
-                lineStyle: LineStyle.Dashed,
-                axisLabelVisible: true,
-                title: 'Цель',
-            }
-
-            const stopLoss = {
-                price: 1.08495,
-                color: 'rgba(247, 40, 40, 0)',
-                lineWidth: '2',
-                lineStyle: LineStyle.Dashed,
-                axisLabelVisible: true,
-                title: 'Стоп',
-            }
-
-            newSeries.createPriceLine(stopLoss);
-            newSeries.createPriceLine(takeProfit);
-            newSeries.createPriceLine(currentPrice);
 
 
             chart.timeScale().fitContent();
@@ -175,16 +224,21 @@ const Chart = ({ data }) => {
             };
 
         },
-        [data]
+        [initialData, barsData]
     );
 
 
 
 
     return (
-        <div className={classes.chart}
-            ref={chartContainerRef}
-        />
+        <>
+            <div className={classes.chart}
+                ref={chartContainerRef}
+            >
+                <div className={classes.border}></div>
+            </div>
+        </>
+
     );
 };
 
